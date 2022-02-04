2ed443d6a82b88109abc22b4e1a5701e5ae24f6f
FatwaArya <fatwaaryasatyaakbar@gmail.com>
2022-02-04 11:19:53 +0700

    4 feb

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express') //import express
 const bodyParser = require('body-parser') //deklarasi body-parser
 const app = express()              //deklarasi variabel express
 const port = 8080                  //deklarasi port
+const { title } =
 
-app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {       // endpoint '/'
@@ -34,6 +36,92 @@ app.put('/orang/:id', (req, res) => {//endpoint put untuk update orang menggunak
     var alamat = req.body.alamat// merequest alamat menggunakan body pada endpoint untuk ditampilkan
     res.end('Seseorang dengan ID' + id + ', telah terupdate')//menggunakan res.end untuk mengakhiri respon dan menampilkan hasil
 });
+
+// data dummy
+let nextId = 4;
+const books = [{
+    id: 1,
+    title: "The First",
+    year: 2019
+},
+{
+    id: 2,
+    title: "The Second",
+    year: 2020
+},
+{
+    id: 3,
+    title: "The Third",
+    year: 2021
+},
+];
+app.get("/books", (req, res) => {
+    res.send({
+        message: "Berhasil menampilkan data buku",
+        data: {
+            books
+        }
+    })
+})
+
+app.post("/books", (req, res) => {
+    const book = {
+        id: nextId++,
+        title: req.body.title,
+        year: req.body.year,
+    }
+    books.push(book);
+    res.send({
+        message: "Berhasil menambahkan buku",
+        data: {
+            newBook: book,
+            totalBooks: books.length,
+        }
+    })
+})
+
+//update data
+app.put("/books/:id", (req, res) => {
+    const id = req.params.id;
+    const book = books.find(book => book.id == id);
+    if (!book) {
+        res.status(404).send({
+            message: "Buku tidak ditemukan"
+        })
+    } else {
+        book.title = req.body.title;
+        book.year = req.body.year;
+        res.send({
+            message: "Berhasil mengubah buku",
+            data: {
+                book,
+                totalBooks: books.length,
+            }
+        })
+    }
+})
+
+//delete data
+app.delete("/books/:id", (req, res) => {
+    const id = req.params.id;
+    const book = books.find(book => book.id == id);
+    if (!book) {
+        res.status(404).send({
+            message: "Buku tidak ditemukan"
+        })
+    } else {
+        const index = books.indexOf(book);
+        books.splice(index, 1);
+        res.send({
+            message: "Berhasil menghapus buku",
+            data: {
+                book,
+                totalBooks: books.length,
+            }
+        })
+    }
+})
+
 
 app.listen(port, () => {//app.listen untuk memulai server pada port 8080
     console.log(`Server di port ${port}`)
